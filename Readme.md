f40fe98280e4b13fc5ebfd344521e23f29575eb6
Sladyn Nunes <sladynnunes98@gmail.com>
2021-02-14 21:48:16 +0530

    Add function to fetch data

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Hawk-eye 
+
+This npm package fetches the status checks for a particular repository and a given pull request number
+
+# Usage
+
+```
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+const { graphql } = require("@octokit/graphql");
+
+async function getStatusData(options, callback) {
+    graphql = graphql.defaults({
+        headers: {
+          authorization: `token ${options.token}`,
+        },
+      });
+
+
+      const query = `{
+        repository(owner: "${options.owner}", name:"${options.repo}"){
+          url
+          pullRequest(number: 162){
+              number
+              url
+              author {
+                login
+                url
+              }
+              commits(last: 1){
+                nodes{
+                  commit{
+                    commitUrl
+                    oid
+                    status {
+                      state
+                      contexts {
+                        state
+                        targetUrl
+                        description
+                        context
+                      }
+                    }
+                  }
+                }
+              }
+          }
+        }
+      }`;
+      
+      try {
+        const result = await graphql(query);
+        callback(result)
+      } catch (error) {
+        console.log("Request failed:", error.request);
+
+      }
+}
+
+module.exports.getStatusData = getStatusData;
